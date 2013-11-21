456e299c4e21e8e11976a92c0e7c04173be30fc4
mejmt <johnlives@gmail.com>
2013-11-21 13:14:00 -0500

    Automatically create the "mudpuppy/cache/" directory for the class location cache if it doesn't exist yet. Also added that directory to gitignore.

--- src/mudpuppy/mudpuppy.php
+++ src/mudpuppy/mudpuppy.php
@@ -2,50 +2,50 @@
 define('MUDPUPPY', true);
 define('MUDPUPPY_VERSION', '1.0.0');
 
-
 // switch to root directory
 chdir(__DIR__);
 chdir('../');
 
 error_reporting(E_ALL);
 
-if (!function_exists('http_response_code'))
-    include_once('mudpuppy/lib/compatibility/httpstatuscode.php');
+if (!function_exists('http_response_code')) {
+	include_once('mudpuppy/lib/compatibility/httpstatuscode.php');
+}
 
-if (!defined('PASSWORD_DEFAULT'))
-    include_once('mudpuppy/lib/compatibility/password.php');
+if (!defined('PASSWORD_DEFAULT')) {
+	include_once('mudpuppy/lib/compatibility/password.php');
+}
 
 // required for exceptions & config options
 require_once('lib/log.php');
 
 if (!include_once('mudpuppy/config.php')) {
-    if (Config::$debug) {
-    	echo 'Cannot start application! Config file is missing.';
-    }
-    http_response_code(500);
-    die();
+	if (Config::$debug) {
+		echo 'Cannot start application! Config file is missing.';
+	}
+	http_response_code(500);
+	die();
 }
 
 if (version_compare(PHP_VERSION, '5.4.0', '<')) {
-    if (Config::$debug) {
-        echo "PHP 5.4.0 or greater is required";
-    }
-    http_response_code(500);
-    die();
+	if (Config::$debug) {
+		echo "PHP 5.4.0 or greater is required";
+	}
+	http_response_code(500);
+	die();
 }
 
 mt_srand((microtime(true) * Config::$randomSeedOffset & 0xFFF) ^ Config::$randomSeedOffset);
 
 // disable error reporting and displaying when not in debug
 if (!Config::$debug) {
-    ini_set('display_errors', '0');
+	ini_set('display_errors', '0');
 	error_reporting(0);
 } else {
 	ini_set('display_errors', '1');
 }
 
 date_default_timezone_set(Config::$timezone);
-
 
 // Activate assert and make it quiet
 assert_options(ASSERT_ACTIVE, 1);
@@ -66,8 +66,12 @@ require_once('lib/file.php');
 /////////////////////////////////////
 function MPAutoLoad($className) {
 	static $classes = null;
-    static $reloadedCache = false;
-	$classcachefile = 'mudpuppy/cache/classlocationcache.php';
+	static $reloadedCache = false;
+	static $cacheDir = 'mudpuppy/cache';
+	if (!file_exists($cacheDir)) {
+		mkdir($cacheDir);
+	}
+	$classCacheFile = "$cacheDir/classlocationcache.php";
 
 	$parts = explode('\\', $className);
 	$class = strtolower($parts[sizeof($parts) - 1]);
@@ -76,34 +80,32 @@ function MPAutoLoad($className) {
 
 	if (is_null($classes)) {
 		// load class location cache
-		if (file_exists($classcachefile)) {
-			include($classcachefile);
+		if (file_exists($classCacheFile)) {
+			include($classCacheFile);
 		}
 	}
 
+	// first check if it is part of a lib with its own autoloader
+	if (strncasecmp($class, 'PHPExcel_', 9) == 0) {
+		return false;
+	}
 
-    // first check if it is part of a lib with its own autoloader
-    if (strncasecmp($class, 'PHPExcel_', 9) == 0) {
-        return false;
-    }
+	if (sizeof($parts) > 0 && in_array(strtolower($parts[0]), array('aws', 'guzzle', 'symfony', 'doctrine', 'psr', 'monolog'))) {
+		// aws.phar must be in the root of the web server
+		// mudpuppy should also be running from the root
+		require_once($_SERVER['DOCUMENT_ROOT'] . '/aws.phar');
+		return false;
+	}
 
-    if (sizeof($parts) > 0 && in_array(strtolower($parts[0]), array('aws', 'guzzle', 'symfony', 'doctrine', 'psr', 'monolog'))) {
-        // aws.phar must be in the root of the web server
-        // mudpuppy should also be running from the root
-        require_once($_SERVER['DOCUMENT_ROOT'].'/aws.phar');
-        return false;
-    }
-
-
-    if (($namespace && (!isset($classes[$namespace]) || !isset($classes[$namespace][$class]) || !file_exists($classes[$namespace][$class])))
+	if (($namespace && (!isset($classes[$namespace]) || !isset($classes[$namespace][$class]) || !file_exists($classes[$namespace][$class])))
 		|| (!$namespace && (!isset($classes[$class]) || !file_exists($classes[$class])))
 	) {
 		// can't find the class, refresh class list
-        if (!$reloadedCache && Config::$debug) {
-            $code = _refreshAutoLoadClasses($classes);
-            File::putContents($classcachefile, $code);
-            $reloadedCache = true;
-        }
+		if (!$reloadedCache && Config::$debug) {
+			$code = _refreshAutoLoadClasses($classes);
+			File::putContents($classCacheFile, $code);
+			$reloadedCache = true;
+		}
 	}
 
 	$file = null;
@@ -118,7 +120,7 @@ function MPAutoLoad($className) {
 		return true;
 	}
 
-	// we failed, this class is not locateable
+	// we failed, this class is not locatable
 	//exception_handler(new Exception("failed to locate class: $className"));
 	return false;
 }
@@ -126,7 +128,7 @@ function MPAutoLoad($className) {
 spl_autoload_register('MPAutoLoad');
 
 function _refreshAutoLoadClasses(&$classes) {
-	Log::add("Refreshing auto-load cache");
+	Log::add('Refreshing auto-load cache');
 	$classes = array();
 
 	// note, modules do not need to be cached as they are loaded on demand without __autoload()
@@ -135,11 +137,11 @@ function _refreshAutoLoadClasses(&$classes) {
 
 	$folders = array_merge(array('mudpuppy/dataobjects/', 'controllers/*', 'mudpuppy/system/', 'mudpuppy/lib/'), Config::$autoloadFolders);
 	foreach ($folders as $folder) {
-        $recursive = false;
-        if (substr($folder, -1) == '*') {
-            $folder = substr($folder, 0, -1);
-            $recursive = true;
-        }
+		$recursive = false;
+		if (substr($folder, -1) == '*') {
+			$folder = substr($folder, 0, -1);
+			$recursive = true;
+		}
 		$files = File::getFileList($folder, $recursive, true, false, '#.*\.php$#');
 		_ralc_parsefiles($classes, $files, $folder);
 	}
@@ -183,9 +185,6 @@ function _assert_handler($file, $line, $code) {
 
 // error handler function
 function error_handler($errNo, $errStr, $errFile, $errLine) {
-	// timestamp for the error entry
-	$dt = date("Y-m-d H:i:s (T)");
-
 	// define an assoc array of error string
 	// in reality the only entries we should
 	// consider are E_WARNING, E_NOTICE, E_USER_ERROR,
@@ -205,8 +204,6 @@ function error_handler($errNo, $errStr, $errFile, $errLine) {
 		E_STRICT => 'Runtime Notice',
 		E_RECOVERABLE_ERROR => 'Catchable Fatal Error'
 	);
-	// set of errors for which a var trace will be saved
-	$user_errors = array(E_USER_ERROR, E_USER_WARNING, E_USER_NOTICE);
 
 	$err = $errorType[$errNo] . ": $errStr in $errFile ($errLine)";
 	switch ($errNo) {
@@ -227,8 +224,6 @@ function error_handler($errNo, $errStr, $errFile, $errLine) {
 
 // exception handler
 function exception_handler(Exception $exception) {
-	$error_data = array('type' => 'Exception', 'errno' => $exception->getCode(), 'message' => $exception->getMessage(),
-		'file' => $exception->getFile(), 'line' => $exception->getLine(), 'trace' => $exception->getTraceAsString());
 	Log::error('An exception has occurred in ' . $exception->getFile() . '(' . $exception->getLine() . "). \nMessage: " . $exception->getMessage());
 
 	if (Config::$debug) {
@@ -245,10 +240,10 @@ function exception_handler(Exception $exception) {
 function shutdown_handler() {
 	$error = error_get_last();
 	if ($error !== null && Config::$debug) {
-		print "SHUTDOWN in file:" . $error['file'] . "(" . $error['line'] . ") - Message:" . $error['message'] . '<br />' . PHP_EOL;
+		print 'SHUTDOWN in file:' . $error['file'] . "(" . $error['line'] . ") - Message:" . $error['message'] . '<br />' . PHP_EOL;
 		Log::displayFullLog();
 	}
-    Log::write();
+	Log::write();
 }
 
 ?>
